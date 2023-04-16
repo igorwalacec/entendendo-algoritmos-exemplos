a267da736a00d18ebf8600189d794e5516ca5de7
Igor Walace Cuevas <icuevas@bankly.com.br>
2023-04-16 13:22:46 -0300

    feat: Implementação do método de busca binária.

---
+++ src/Algoritmos/Buscas.cs
@@ -0,0 +1,25 @@
+﻿namespace Algoritmos;
+
+public class Buscas
+{
+    public int? PesquisaBinaria<T>(IEnumerable<T> lista, T item) where T : IComparable<T>
+    {
+        var baixo = 0;
+        var alto = lista.Count() - 1;
+        while (baixo <= alto)
+        {
+            var meio = (baixo + alto) / 2;
+            var chute = lista.ElementAt(meio);
+
+            var comparacao = item.CompareTo(chute);
+            if (comparacao == 0)
+                return meio;
+            if (comparacao < 0)
+                alto = meio - 1;
+            else
+                baixo = meio + 1;
+        }
+
+        return null;
+    } 
+}
---
+++ tests/Algoritmos.Tests/BuscasTests.cs
@@ -0,0 +1,40 @@
+namespace Algoritmos.Tests;
+
+public class BuscasTests
+{
+    private readonly Buscas _buscas;
+
+    public BuscasTests()
+    {
+        _buscas = new Buscas();
+    }
+
+    [Fact(DisplayName = "Deve encontrar a posição correta do item na lista de inteiros"),Trait("Busca Binária","Buscas")]
+    public void DeveEncontrarAPosicaoDoItemDentroDaListaDeInteiros()
+    {
+        var listaNumeros = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+
+        var resultado = _buscas.PesquisaBinaria(listaNumeros, 7);
+        
+        Assert.Equal(6, resultado);
+    }
+    [Fact(DisplayName = "Deve encontrar a posição correta do item na lista de strings"),Trait("Busca Binária","Buscas")]
+    public void DeveEncontrarAPosicaoDoItemDentroDaListaDeStrings()
+    {
+        var listaNumeros = new List<string> { "Andre", "Biel", "Carlos", "Daniel", "Ederson" };
+
+        var resultado = _buscas.PesquisaBinaria(listaNumeros, "Andre");
+        
+        Assert.Equal(0, resultado);
+    }
+
+    [Fact(DisplayName = "Deve retornar null quando item não for encontrado na lista"),Trait("Busca Binária","Buscas")]
+    public void DeveRetornarNullQuandoItemNaoEncontrado()
+    {
+        var listaNumeros = new List<string> { "Andre", "Biel", "Carlos", "Daniel", "Ederson" };
+
+        var resultado = _buscas.PesquisaBinaria(listaNumeros, "Igor");
+        
+        Assert.Equal(null, resultado);
+    }
+}
